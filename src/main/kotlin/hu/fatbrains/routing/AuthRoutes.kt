734400777bfabd065af02f32ba9fe5e6dc7cab2f
jazzysnake <dotya77@gmail.com>
2022-03-19 13:45:38 +0100

    logging added

--- src/main/kotlin/hu/fatbrains/routing/AuthRoutes.kt
+++ src/main/kotlin/hu/fatbrains/routing/AuthRoutes.kt
@@ -21,10 +21,12 @@ fun Route.authRoutes(application: Application,kodein: Kodein){
         val password = params["password"]
         val user = userDs.getUserByEmail(params["email"].toString())
         if (email==null||password==null){
-            call.respondText("Provide a email and a password!")
+            application.log.debug("Failed login attempt, bad params provided. Params: $params")
+            call.respondText("Provide a email and a password!", status = HttpStatusCode.BadRequest)
         }
         else if (user==null){
-            call.respondText("No user registered with email: $email")
+            application.log.debug("Failed login attempt, no user registered with provided email. Params: $params")
+            call.respondText("No user registered with email: $email", status = HttpStatusCode.BadRequest)
         }
         else if (BCrypt.checkpw(password,user.password)) {
             val session = call.sessions.get<UserSession>()
@@ -33,9 +35,13 @@ fun Route.authRoutes(application: Application,kodein: Kodein){
                 call.respond(HttpStatusCode.ExpectationFailed, "NO SESSION")
             } else {
                 call.sessions.set(session.copy(userId = user.id))
-                call.respondText("Welcome ${user.name}")
+                application.log.info("Logged-in with user:${user.id}")
+                call.respondText("Welcome ${user.name}", status = HttpStatusCode.OK)
             }
-        }else call.respondText("Bad password")
+        }else {
+            application.log.info("Unsuccessful login attempt by user: ${user.id}")
+            call.respondText("Incorrect password")
+        }
 
     }
     post("/register") {
@@ -44,7 +50,8 @@ fun Route.authRoutes(application: Application,kodein: Kodein){
         val email = params["email"]
         val password = params["password"]
         if (username==null||password==null||email==null){
-            call.respondText("Provide a username a password and a valid email")
+            application.log.debug("Failed registration attempt, bad params provided. Params: $params")
+            call.respondText("Provide a username a password and a valid email", status = HttpStatusCode.BadRequest)
         }
         else{
             if(userDs.getUserByEmail(email)==null){
@@ -55,16 +62,19 @@ fun Route.authRoutes(application: Application,kodein: Kodein){
                     contactIds = listOf(),
                     roomIds = listOf(),
                 ))
-                call.respondText("Registered user: $username")
+                application.log.info("Successfully registered new user: $username")
+                call.respondText("Registered user: $username", status = HttpStatusCode.OK)
             }
             else{
+                application.log.info("Unsuccessful user registration, email: $email already tied to an account.")
                 call.respondText("$email is already registered")
             }
         }
     }
     get("/logout"){
+        application.log.info("Logged out user: ${call.sessions.get<UserSession>()?.userId}")
         call.sessions.clear<UserSession>()
-        call.respondText("Goodbye")
+        call.respondText("Goodbye", status = HttpStatusCode.OK)
     }
 
 }
